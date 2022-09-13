d0410c5aaec29fe073aac9eaa5a20de9f8c2ed83
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-09-13 14:38:47 -0400

    Update post in users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var { validateUserData } = require('../validation/users')
+
+const userList = [];
+
 /* GET users listing. */
-router.get('/all', function(req, res, next) {
+router.get('/all', (req, res) => {
   res.send('respond with a resource');
 });
 
@@ -10,6 +14,43 @@ router.get('/single', (req, res) => {
   res.json()({
     success: true,
     user: "Single User"
+  })
+})
+
+
+router.post(',create-one', (req, res) => {
+  const email = req.body.email
+  const firstName = req.body.firstName
+  const lastName = req.body.lastName
+  const age = req.body.age
+  const favoriteFood = req.body.favoriteFood
+
+  const userData = {
+    email,
+    firstName,
+    lastName,
+    age,
+    favoriteFood,
+    fullName: firstName + lastName,
+    createdAt: new Date(),
+    lastModified: new Date()
+  }
+  const userDataCheck = validateUserData(userData);
+
+  if (userDataCheck.isValid === false){
+    res.json({
+      success: false,
+      message: userDataCheck.message
+    })
+    return;
+  }
+
+  userList.push(userData)
+
+  console.log("userList", userList)
+
+  res.json({
+    success: true
   })
 })
 
